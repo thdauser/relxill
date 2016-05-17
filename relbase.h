952fa07f9b1a253e6d23db786d68efd5ed47c070
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2016-05-17 14:55:03 +0200

    initial commit

---
+++ relbase.h
@@ -0,0 +1,28 @@
+/*
+   This file is part of the RELXILL model code.
+
+   RELXILL is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   RELXILL is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+   For a copy of the GNU General Public License see
+   <http://www.gnu.org/licenses/>.
+
+    Copyright 2016 Thomas Dauser, Remeis Observatory & ECAP
+*/
+#ifndef RELBASE_H_
+#define RELBASE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <assert.h>
+
+
+#endif /* RELBASE_H_ */
