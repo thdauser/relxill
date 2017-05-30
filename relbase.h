51c93afb2d70259fb9d4d9df45d7ab3f35f28562
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2017-05-30 14:58:53 +0200

    [0.2.5dev] fixed lower limits of xillver-ecut and kte in lmodel.dat file

--- relbase.h
+++ relbase.h
@@ -36,7 +36,7 @@
 
 #define version_major 0
 #define version_minor 2
-#define version_build 4
+#define version_build 5
 #define version_dev "dev"
 
 // TODO: allow to set it by an environment variable
