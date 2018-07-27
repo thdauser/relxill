01c862f1ded09a1f9fbb05c299b898225af8099c
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2018-07-27 11:28:43 +0200

    updates copyright to 2018

--- relutility.h
+++ relutility.h
@@ -13,7 +13,7 @@
    For a copy of the GNU General Public License see
    <http://www.gnu.org/licenses/>.
 
-    Copyright 2016 Thomas Dauser, Remeis Observatory & ECAP
+    Copyright 2018 Thomas Dauser, Remeis Observatory & ECAP
 */
 #ifndef RELUTILITY_H_
 #define RELUTILITY_H_
