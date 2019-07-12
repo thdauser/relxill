4fbf20f0eae273a3e67daccc05573f7f8e6b5971
Thomas Dauser <Thomas.Dauser@sternwarte.uni-erlangen.de>
2019-07-12 08:00:22 +0200

    updates copyright to 2019

--- rellp.h
+++ rellp.h
@@ -13,7 +13,7 @@
    For a copy of the GNU General Public License see
    <http://www.gnu.org/licenses/>.
 
-    Copyright 2018 Thomas Dauser, Remeis Observatory & ECAP
+    Copyright 2019 Thomas Dauser, Remeis Observatory & ECAP
 */
 #ifndef RELLP_H_
 #define RELLP_H_
