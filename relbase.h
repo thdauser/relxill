3e330ab8ce7f930e485c0fe03c12e94af6eae3ed
Thomas Dauser <thomas.dauser@fau.de>
2020-06-22 11:18:33 +0200

    [1.3.9] offical vers update, fixing float-double bug

--- relbase.h
+++ relbase.h
@@ -35,8 +35,8 @@
 
 #define version_major 1
 #define version_minor 3
-#define version_build 8
-#define version_dev "-3"
+#define version_build 9
+#define version_dev ""
 
 /** path to all RELXILL tables */
 #define RELXILL_TABLE_PATH "./"
