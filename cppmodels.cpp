f8b4eae13582308b8855de57a258ec8cae16001f
Thomas Dauser <thomas.dauser@fau.de>
2020-12-02 10:18:01 +0100

    moves functions to .cpp file

--- cppmodels.cpp
+++ cppmodels.cpp
@@ -22,6 +22,44 @@
 #include <stdexcept>
 #include <iostream>
 
+void LocalModel::line_model(CppSpectrum &spectrum) {
+
+  // get the relevant parameters
+  relParam *rel_param = getRelParamStruct(m_param, m_name, m_info);
+
+  // shift the spectrum such that we can calculate the line for 1 keV
+  spectrum.shift_energy_grid_1keV(rel_param->lineE, rel_param->z);
+
+  int status = EXIT_SUCCESS;
+
+  relline_base(spectrum.energy_double(), spectrum.flux_double(), spectrum.nener_bins(), rel_param, &status);
+
+  if (status != EXIT_SUCCESS) {
+    throw std::exception();
+  }
+
+  // need to (internally) copy the values from the double-array to the Array structures
+  spectrum.copy_doubleArrays2array();
+
+}
+
+void relxill_model(const CppSpectrum &spectrum) {
+  //  relxill_kernel(ener, flux, n_ener0, xill_param, rel_param, status);
+  //  double *ener_shifted = shift_energ_spec_1keV(ener0, n_ener0, 1.0, rel_param->z, status);
+  //  rebin_spectrum(ener_shifted, photar, n_ener0, ener, flux, n_ener0);
+}
+
+void conv_model(const CppSpectrum &spectrum) {
+  // shift the spectrum such that we can calculate the line for 1 keV
+  //  double *ener1keV = shift_energ_spec_1keV(ener, n_ener, param_struct->lineE, param_struct->z, status);
+  //  CHECK_STATUS_VOID(*status);
+  //
+  //  relconv_kernel(ener1keV, photar, n_ener, rel_param, status);
+}
+
+void xillver_model(const CppSpectrum &spectrum) {
+  //  xillver_base(ener0, n_ener0, photar, xill_param, status);
+}
 
 void xspec_wrapper_eval_model(ModelName model_name, const Array &energy, Array &flux, const Array &parameter) {
 
