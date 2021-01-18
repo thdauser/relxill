3a79dba315eae9a441ed7819342c05a107182ee1
Thomas Dauser <thomas.dauser@fau.de>
2021-01-18 23:36:15 +0100

    adds empty new files

---
+++ src/writeOutfiles.h
@@ -0,0 +1,21 @@
+/*
+   This file is part of the RELXILL model code.
+
+   RELXILL is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   RELXILL is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+   For a copy of the GNU General Public License see
+   <http://www.gnu.org/licenses/>.
+
+    Copyright 2020 Thomas Dauser, Remeis Observatory & ECAP
+*/
+#ifndef WRITEOUTFILES_H_
+#define WRITEOUTFILES_H_
+
+#endif 
