0ee977c645bb5b1c7787cb87694888b579b690e9
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2022-04-06 10:34:43 +0200

    updates filename in compile-script (from change to cpp)

--- src/modelfiles/compile_relxill.sh
+++ src/modelfiles/compile_relxill.sh
@@ -2,7 +2,7 @@
 
 ln -sf lmodel_relxill.dat lmodel.dat
 
-sed -i "s,#define RELXILL_TABLE_PATH.*,#define RELXILL_TABLE_PATH "'"'`pwd`'"'"," relbase.h
+sed -i "s,#define RELXILL_TABLE_PATH.*,#define RELXILL_TABLE_PATH "'"'`pwd`'"'"," Relbase.h
 
 echo "initpackage relxill lmodel_relxill.dat `pwd` \nexit" | xspec
 echo "lmod relxill . \nexit" | xspec
