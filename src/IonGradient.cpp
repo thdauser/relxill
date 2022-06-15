2aa40080aa2f6363e0c53c559fedd6a4e0991ef5
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2022-06-15 22:18:08 +0200

    updates copyright to 2022

--- src/IonGradient.cpp
+++ src/IonGradient.cpp
@@ -13,7 +13,7 @@
    For a copy of the GNU General Public License see
    <http://www.gnu.org/licenses/>.
 
-    Copyright 2021 Thomas Dauser, Remeis Observatory & ECAP
+    Copyright 2022 Thomas Dauser, Remeis Observatory & ECAP
 */
 #include "IonGradient.h"
 #include "Relbase.h"
