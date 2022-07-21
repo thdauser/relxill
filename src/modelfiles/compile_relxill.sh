ac4bb3588d56da3c5f044e6bdeb69704aa02c27d
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2022-07-21 12:55:27 +0200

    [20210721] updates compile script (sed call was broken)

--- src/modelfiles/compile_relxill.sh
+++ src/modelfiles/compile_relxill.sh
@@ -2,10 +2,10 @@
 
 ln -sf lmodel_relxill.dat lmodel.dat
 
-sed -i "s,#define RELXILL_TABLE_PATH.*,#define RELXILL_TABLE_PATH "'"'`pwd`'"'"," Relbase.h
+sed -i "s,#define RELXILL_TABLE_PATH.*,#define RELXILL_TABLE_PATH "'"'`pwd`'"'"," common.h
 
 #  "For Mac OSX systems, you might have to use this"
-# sed -i.ori "s,#define RELXILL_TABLE_PATH.*,#define RELXILL_TABLE_PATH "'"'`pwd`'"'"," Relbase.h
+# sed -i.ori "s,#define RELXILL_TABLE_PATH.*,#define RELXILL_TABLE_PATH "'"'`pwd`'"'"," common.h
 
 echo "initpackage relxill lmodel_relxill.dat `pwd` \nexit" | xspec
 echo "lmod relxill . \nexit" | xspec
