7fad93b363cabf1cd18fb1a369ebbccae7b67050
Thomas Dauser <thomas.dauser@sternwarte.uni-erlangen.de>
2022-08-26 10:13:07 +0200

    adds simple test for the alpha-model

---
+++ test/unit/tests-alpha-model.cpp
@@ -0,0 +1,35 @@
+/*
+   This file is part of the RELXILL model code.
+
+   RELXILL is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
+
+   RELXILL is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+   For a copy of the GNU General Public License see
+   <http://www.gnu.org/licenses/>.
+
+    Copyright 2021 Thomas Dauser, Remeis Observatory & ECAP
+*/
+
+#include "catch2/catch_amalgamated.hpp"
+#include "LocalModel.h"
+#include "xspec_wrapper_lmodels.h"
+#include "XspecSpectrum.h"
+#include "common-functions.h"
+
+#include <vector>
+#include <filesystem>
+
+TEST_CASE(" Execute relxillpAlpha", "[alpha]") {
+  DefaultSpec default_spec{};
+
+  LocalModel lmod(ModelName::relxilllpAlpha);
+
+  auto spec = default_spec.get_xspec_spectrum();
+  REQUIRE_NOTHROW(lmod.eval_model(spec));
+}
